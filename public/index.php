9649c39027b769414382408130b23ee6f3473fb2
Josh Cirre <joshcirre@gmail.com>
2024-02-19 11:37:21 -0700

    feat: first init

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new FrameworkX\App();
+
+$app->get('/', function () {
+    return React\Http\Message\Response::plaintext(
+        "Hello wörld!\n"
+    );
+});
+
+$app->get('/users/{name}', function (Psr\Http\Message\ServerRequestInterface $request) {
+    return React\Http\Message\Response::plaintext(
+        "Hello " . $request->getAttribute('name') . "!\n"
+    );
+});
+
+$app->run();
